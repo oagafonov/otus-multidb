0a4d76e8b2856c5d169b21174febec9944a8e439
Oleg Agafonov <oleg.a.agafonov@gmail.com>
2024-02-29 21:39:45 +0300

    done: uses env variable + improvement

--- src/main/java/com/example/otusmultidb/OtusMultidbApplication.java
+++ src/main/java/com/example/otusmultidb/OtusMultidbApplication.java
@@ -2,8 +2,10 @@ package com.example.otusmultidb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class OtusMultidbApplication {
 
   public static void main(String[] args) {
